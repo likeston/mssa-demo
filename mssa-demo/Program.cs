175874fda17896d362dd5fd666ad2606721beacf
likeston <likeston@microsoft.com>
2021-08-23 13:58:30 -0400

    changed hello world message

--- mssa-demo/Program.cs
+++ mssa-demo/Program.cs
@@ -6,7 +6,7 @@ namespace mssa_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again!");
         }
     }
 }
