644f9c5a96be1a865b05cb77d1a3b1ad8186446d
likeston <likeston@microsoft.com>
2021-08-23 11:23:01 -0400

    add console app files

---
+++ mssa-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mssa_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
